b959c4329916b73bdb4cd91b3d5fd62d2f6433d2
SoulMinT05 <minhtam5111993@gmail.com>
2024-09-19 22:02:44 +0700

    Cau hinh ESLint cho du an

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use('/api/contacts', contactRouter);
 app.use((req, res, next) => {
     return next(new ApiError(404, 'Resource not found'));
 });
-
 app.use((error, req, res, next) => {
     return res.status(error.statusCode || 500).json({
         message: error.message || 'Internal Server Error',
